dcf42d2f4cb40d8f9de721dab132e00e18ceaa35
Alexandre Blache <alexandreblache.pro@gmail.com>
2021-01-15 15:44:13 +0100

    api basique

---
+++ src/main/java/com/example/test/student/Student.java
@@ -0,0 +1,48 @@
+package com.example.test.student;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+@Entity
+@Table
+public @Data
+class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate anniv;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(Long id,
+                   String name,
+                   String email,
+                   LocalDate anniv
+                  ) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.anniv = anniv;
+    }
+
+    public Student(String name,
+                   String email,
+                   LocalDate anniv
+                   ) {
+        this.name = name;
+        this.email = email;
+        this.anniv = anniv;
+    }
+
+    public int getAge() {
+        return Period.between(anniv, LocalDate.now()).getYears();
+    }
+}
---
+++ src/main/java/com/example/test/student/StudentService.java
@@ -0,0 +1,72 @@
+package com.example.test.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    };
+
+    public void addStudent(Student student) {
+        Optional<Student> studentExist = studentRepository.findStudentByEmail(student.getEmail());
+
+        if(studentExist.isPresent())
+            throw new IllegalStateException("Adresse email déjà utilisée");
+        else
+            studentRepository.save(student);
+    }
+
+    public void deleteById(Long id) {
+        boolean studentExist = studentRepository.existsById(id);
+
+        if(!studentExist)
+            throw new IllegalStateException("Cet étudiant n'existe pas!");
+        else
+            studentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email)
+    {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("L'étudiant " + studentId + " n'existe pas"));
+
+
+//        if (student.getName().equals(name)) throw new IllegalStateException("erreur");
+
+        if(name != null &&
+                name.length()>0 &&
+                !student.getName().equals(name)) {
+            student.setName(name);
+        }
+
+        if(email != null &&
+                email.length()>0 &&
+                !student.getEmail().equals(email)) {
+            Optional<Student> studentEmail = studentRepository.findStudentByEmail(email);
+            if(studentEmail.isPresent() ) {
+                throw new IllegalStateException("Cet email est déjà utilisé");
+            } else {
+                student.setEmail(email);
+            }
+
+        }
+    }
+}
