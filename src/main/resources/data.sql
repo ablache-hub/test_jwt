8edaa28729af99c2e3d5c0d728fe4fa3729f8d92
Alexandre Blache <alexandreblache.pro@gmail.com>
2021-01-22 10:19:00 +0100

    data.sql + properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO `app_user`
+(`id`,
+ `app_user_role`,
+ `email`,
+ `enabled`,
+ `first_name`,
+ `last_name`,
+ `locked`,
+ `password`)
+
+VALUES
+('1',
+ 'USER',
+ 'a.blache@gmail.com',
+ 1,
+ 'alexandre',
+ 'blache',
+ 0,
+ '$2a$10$bWNZ/hQo/1KAm8bkDxS3sudVBqHJFJjDw0oMJ1V3hidKXGDmc8q.q');
